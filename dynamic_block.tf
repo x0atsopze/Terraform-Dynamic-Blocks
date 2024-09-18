e65deb9db06d1d1f1c5005663ed9d1cd90d689a0
x0atsopze <109428762+x0atsopze@users.noreply.github.com>
2024-09-18 09:16:55 -0400

    Update dynamic_block.tf

--- dynamic_block.tf
+++ dynamic_block.tf
@@ -2,13 +2,15 @@ resource "aws_security_group" "dynamicsg" {
   name        = "dynamic-sg"
   description = "Ingress for Vault"
 
-#create the dynamic ingree and iterate in the sg_ports variable
+#create the dynamic ingrees and iterate in the sg_ports variable
   dynamic "ingress" {
     for_each = var.sg_ports
-    iterator = port #if no iterator is defined, the name of the variable defaults to the label of the dynamic block which is ingress in this case
-    content {               
-      from_port   = port.value #if there were no iterator port up there, this value
-      to_port     = port.value #would have been ingress.value
+#if no iterator is defined, the name of the variable defaults to the label of the dynamic block which is ingress in this case
+    iterator = port 
+    content {
+#if there were no iterator port up there, this value would have been ingress.value
+      from_port   = port.value 
+      to_port     = port.value
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
@@ -17,7 +19,8 @@ resource "aws_security_group" "dynamicsg" {
   dynamic "egress" {
     for_each = var.sg_ports
     content {
-      from_port   = egress.value #no iterator defined so the value here is label of dynamic block + value
+#no iterator defined so the value here is label of dynamic block + value
+      from_port   = egress.value 
       to_port     = egress.value
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
